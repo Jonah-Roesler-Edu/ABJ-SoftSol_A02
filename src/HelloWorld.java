f1b7b4ba8a2c630fcf590ee296bd25c3a9462b68
Roesler <roeslerj1@student.douglascollege.ca>
2019-01-30 17:05:43 -0800

    Added comment

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
 		System.out.println("Hello World!");
 		System.out.println("Hello Alyssa Jia!");
 		System.out.println("Hello Jonah Roesler!");
+		//Hello from Jonah 2019-01-30 4:46 PM
 	}
 }
