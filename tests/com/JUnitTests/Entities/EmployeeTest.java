ddffa4ba392ecc8c8bfc694bbb96f2f385695078
Jia <jiaa@student.douglascollege.ca>
2019-03-24 16:48:09 -0700

    Added Employee and Task JUnit Tests

--- tests/com/JUnitTests/Entities/EmployeeTest.java
+++ tests/com/JUnitTests/Entities/EmployeeTest.java
@@ -4,11 +4,69 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import hibernatePack.Employee;
+
 class EmployeeTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testEmail() {
+		Employee Employee = new Employee();
+		Employee.setEmail("TestEmail");
+		String test = Employee.getEmail();
+		
+		assertTrue(test.equals("TestEmail"));
+		
+		//System.out.println("Works for Email");
+	}
+	@Test
+	void testFirstName() {
+		Employee Employee = new Employee();
+		Employee.setFName("TestFirstName");
+		String test = Employee.getFName();
+		
+		assertTrue(test.equals("TestFirstName"));
+		
+		//System.out.println("Works for First Name");
+	}
+	@Test
+	void testLastName() {
+		Employee Employee = new Employee();
+		Employee.setLName("TestFirstName");
+		String test = Employee.getLName();
+		
+		assertTrue(test.equals("TestFirstName"));
+		
+		//System.out.println("Works for Last Name");
+	}
+	@Test
+	void testPassword() {
+		Employee Employee = new Employee();
+		Employee.setPass("TestPassword");
+		String test = Employee.getPass();
+		
+		assertTrue(test.equals("TestPassword"));
+		
+		//System.out.println("Works for Password");
+	}
+	@Test
+	void testPhone() {
+		Employee Employee = new Employee();
+		Employee.setPhone("TestPhone");
+		String test = Employee.getPhone();
+		
+		assertTrue(test.equals("TestPhone"));
+		
+		//System.out.println("Works for Phone");
+	}
+	@Test
+	void testAddress() {
+		Employee Employee = new Employee();
+		Employee.setAddress("TestAddress");
+		String test = Employee.getAddress();
+		
+		assertTrue(test.equals("TestAddress"));
+		
+		//System.out.println("Works for Address");
 	}
 
 }
--- tests/com/JUnitTests/Entities/TaskTest.java
+++ tests/com/JUnitTests/Entities/TaskTest.java
@@ -2,13 +2,76 @@ package com.JUnitTests.Entities;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDateTime;
+
 import org.junit.jupiter.api.Test;
+
+import hibernatePack.Task;
 
 class TaskTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testCreatorEmail() {
+		Task task = new Task();
+		task.setCreatorEmail("TestCreatorEmail");
+		String test = task.getCreator();
+		
+		assertTrue(test.equals("TestCreatorEmail"));
+		
+		//System.out.println("Works for Creator Email");
+	}
+	@Test
+	void testWorkerEmail() {
+		Task task = new Task();
+		task.setWorkerEmail("TestWorkerEmail");
+		String test = task.getWorker();
+		
+		assertTrue(test.equals("TestWorkerEmail"));
+		
+		//System.out.println("Works for Worker Email");
+	}
+	@Test
+	void testTaskText() {
+		Task task = new Task();
+		task.setTaskText("TestTaskText");
+		String test = task.getTaskText();
+		
+		assertTrue(test.equals("TestTaskText"));
+		
+		//System.out.println("Works for Task Text");
+	}
+	@Test
+	void testAssignDate() {
+		Task task = new Task();
+		LocalDateTime date = LocalDateTime.parse("2007-12-03T10:15:30");
+		task.setDateAssign(date);
+		LocalDateTime test = task.getDateAssign();
+		
+		assertTrue(test.equals(date));
+		
+		//System.out.println("Works for Assign Date");
+	}
+	@Test
+	void testDueDate() {
+		Task task = new Task();
+		
+		LocalDateTime date = LocalDateTime.parse("2007-12-03T10:15:30");
+		task.setDateDue(date);
+		LocalDateTime test = task.getDateDue();
+		
+		assertTrue(test.equals(date));
+		
+		//System.out.println("Works for Due Date");
+	}
+	@Test
+	void testStatus() {
+		Task task = new Task();
+		task.setStatus("TestStatus");
+		String test = task.getStatus();
+		
+		assertTrue(test.equals("TestStatus"));
+		
+		//System.out.println("Works for Status");
 	}
 
 }
